8ad4b42ded5075a9663778cfde735941351c0d5a
Alex Gaudi <itehnoviking@gmail.com>
2023-10-29 00:50:58 +0300

    Add project files.

---
+++ SignalRTestApp/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTestApp;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string message, string userName)
+    {
+        await Clients.All.SendAsync("Receive", message, userName);
+    }
+}
---
+++ SignalRTestApp/Program.cs
@@ -0,0 +1,14 @@
+using SignalRTestApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();  
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");  
+
+app.Run();
